a0259f2ba8bd6afb532fdd6bea05d98b1d47fd0c
Alex Mozolevkiy <mozolevskiy2002@mail.ru>
2025-02-18 22:27:14 +0300

    Докинул зависимостей, теперь должен работать как Spring проект

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -1,7 +1,17 @@
 package domain;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
 public class Main {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
         System.out.println("Hello team!");
     }
 }
