bbc16ab3cbf72b58af8d11fa7ed976380c9dbc86
Daria <procionestrisce@yandex.ru>
2025-02-19 20:54:30 +0300

    add taskServices

---
+++ src/main/java/domain/dto/TaskDto.java
@@ -0,0 +1,7 @@
+package ru.aston.training.Task.dto;
+
+/**
+ * Created by Lorden on 19.02.2025
+ */
+public class TaskDto {
+}
---
+++ src/main/java/domain/entity/Task.java
@@ -0,0 +1,7 @@
+package ru.aston.training.Task.entity;
+
+/**
+ * Created by Lorden on 19.02.2025
+ */
+public class Task {
+}
