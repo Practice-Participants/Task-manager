026fb34536f637774cb48e1be2df97a3ba3e0a16
basikMisha <michail.rastigaev@gmail.com>
2025-02-25 20:25:12 +0300

    fix database saving

--- src/main/java/domain/model/LogEntry.java
+++ src/main/java/domain/model/LogEntry.java
@@ -1,6 +1,8 @@
 package domain.model;
+
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "logs")
 public class LogEntry {
@@ -10,12 +12,29 @@ public class LogEntry {
 
     private String eventType;
     private String eventData;
-    private LocalDateTime timestamp = LocalDateTime.now();
+    private LocalDateTime timestamp;
 
     public LogEntry() {}
 
     public LogEntry(String eventType, String eventData) {
-        this.eventData = eventData;
         this.eventType = eventType;
+        this.eventData = eventData;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public String getEventData() {
+        return eventData;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
     }
 }
--- src/main/java/domain/service/KafkaConsumerService.java
+++ src/main/java/domain/service/KafkaConsumerService.java
@@ -1,20 +1,35 @@
 package domain.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.model.LogEntry;
 import domain.repository.LogRepository;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 @Service
 public class KafkaConsumerService {
     private final LogRepository logRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     public KafkaConsumerService(LogRepository logRepository) {
         this.logRepository = logRepository;
     }
+
     @KafkaListener(topics = "audit-log-topic", groupId = "audit-log-group")
     public void consume(ConsumerRecord<String, String> record) {
-        LogEntry logEntry = new LogEntry(record.key(), record.value());
-        logRepository.save(logEntry);
-        System.out.println("Received: " + record.value());
+        try {
+            JsonNode jsonNode = objectMapper.readTree(record.value());
+            String eventType = jsonNode.get("eventType").asText();
+            String eventData = jsonNode.get("eventData").toString();
+
+            LogEntry logEntry = new LogEntry(eventType, eventData);
+            logRepository.save(logEntry);
+
+            System.out.println("Received and saved: " + record.value());
+        } catch (Exception e) {
+            System.err.println("Error processing Kafka message: " + e.getMessage());
+        }
     }
 }
