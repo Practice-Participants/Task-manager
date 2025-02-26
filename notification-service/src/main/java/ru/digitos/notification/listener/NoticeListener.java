eef7abb521e6f5c8da556777d455be560b72999a
”XFQDesNik” <xfqdesnik@mail.ru>
2025-02-26 22:10:05 +0300

    Test inject from kafka

--- notification-service/src/main/java/ru/digitos/notification/listener/NoticeListener.java
+++ notification-service/src/main/java/ru/digitos/notification/listener/NoticeListener.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Slf4j
 @Component
 public class NoticeListener {
     //@KafkaListener(topics = "notice-events")
     @KafkaListener(topics = "task-service")
-    public void listen(String payload){
-        log.info("Recived notice: {}", payload);
+    public void listen(Map task){
+        log.info("Recived title: {}", task.get("title"));
+        log.info("Recived userId: {}", task.get("userId"));
+        log.info("Recived status: {}", task.get("status"));
     }
 }
