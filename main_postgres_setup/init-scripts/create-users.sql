bbabca8557f793498c25a651087840dcfb00fead
Sawa <mozolevskiy2002@mail.ru>
2025-03-01 23:38:57 +0300

    Delete main_postgres_setup directory

--- main_postgres_setup/init-scripts/create-users.sql
+++
@@ -1,7 +0,0 @@
--- Создание пользователей
-CREATE USER logservice WITH PASSWORD 'logservice';
-CREATE USER notification WITH PASSWORD 'notification-pwd';
-
--- Назначение привилегий
-GRANT ALL PRIVILEGES ON DATABASE postgres TO logservice;
-GRANT ALL PRIVILEGES ON DATABASE postgres TO notification;
